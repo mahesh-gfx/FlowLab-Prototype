941f41c92bd0ea35c622a99ce6f3458b07eea3bb
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2024-07-23 05:36:18 +0100

    feat: added mini map component, updated position of controls component

--- packages/frontend/src/components/WorkflowCanvas.tsx
+++ packages/frontend/src/components/WorkflowCanvas.tsx
@@ -14,6 +14,7 @@ import ReactFlow, {
   Position,
   ReactFlowInstance,
   NodeChange,
+  MiniMap,
 } from "reactflow";
 import "reactflow/dist/style.css";
 import {
@@ -369,8 +370,9 @@ const WorkflowCanvas: React.FC = () => {
           nodeTypes={nodeTypes}
           fitView
         >
-          <Controls />
+          <Controls position="top-right" />
           <Background />
+          <MiniMap position="bottom-right" />
         </ReactFlow>
         <NodePanel nodeTypes={nodeDefinitions} onDragStart={onDragStart} />
         <div
