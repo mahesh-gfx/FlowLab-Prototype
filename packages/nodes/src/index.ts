5d25a52cfea7749785351f4a6f26e8c2d58d5cfb
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2024-07-24 18:33:47 +0100

    Chore: removed CSVtoJSONNode, not required

--- packages/nodes/src/index.ts
+++ packages/nodes/src/index.ts
@@ -2,5 +2,4 @@ export * from "./BaseNode";
 export * from "./nodeTypes/StartNode";
 export * from "./nodeTypes/ReadCSVNode";
 export * from "./nodeTypes/InputNode";
-export * from "./nodeTypes/CSVToJSONNode";
 // Export other node types as you create them
