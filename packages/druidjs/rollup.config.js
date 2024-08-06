f44a0dfff4f520e9237f8cb0ee7dd01e73e1be57
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2024-08-06 18:52:10 +0100

    Fix: reconfigured druid build to be in .cjs

--- packages/druidjs/rollup.config.js
+++ packages/druidjs/rollup.config.js
@@ -1,75 +1,69 @@
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
 import json from "@rollup/plugin-json";
 
 const onwarn = (message, warn) => {
-    if (message.code === "CIRCULAR_DEPENDENCY") return;
-    warn(message);
+  if (message.code === "CIRCULAR_DEPENDENCY") return;
+  warn(message);
 };
 
 export default [
-    {
-        input: "src/index.js",
-        output: {
-            sourcemap: true,
-            extend: true,
-            file: "dist/druid.js",
-            format: "umd",
-            indent: false,
-            name: "druid",
-        },
-        plugins: [
-            json({
-                compact: true,
-                exclude: "node_modules/**",
-            }),
-            resolve(),
-        ],
-        onwarn,
+  {
+    input: "src/index.js",
+    output: {
+      sourcemap: true,
+      extend: true,
+      file: "dist/druid.js",
+      format: "umd",
+      indent: false,
+      name: "druid",
     },
-    {
-        input: "src/index.js",
-        plugins: [
-            json({
-                compact: true,
-            }),
-            resolve(),
-            terser(),
-        ],
-        output: {
-            sourcemap: true,
-            extend: true,
-            file: "dist/druid.min.js",
-            format: "umd",
-            indent: false,
-            name: "druid",
-        },
-        onwarn,
-    } /* 
-    {
-        input: "index.js",
-        plugins: [
-            json({
-                compact: true,
-            }),
-            resolve(),
-            terser({
-                format: {
-                    comments: "all",
-                },
-                keep_classnames: true,
-                keep_fnames: true,
-                compress: true,
-            }),
-        ],
-        output: {
-            sourcemap: true,
-            extend: true,
-            file: "dist/druid.esm.js",
-            format: "es",
-            indent: false,
-            name: "druid",
-        },
-        onwarn,
-    }, */,
+    plugins: [
+      json({
+        compact: true,
+        exclude: "node_modules/**",
+      }),
+      resolve(),
+    ],
+    onwarn,
+  },
+  {
+    input: "src/index.js",
+    plugins: [
+      json({
+        compact: true,
+      }),
+      resolve(),
+      terser(),
+    ],
+    output: {
+      sourcemap: true,
+      extend: true,
+      file: "dist/druid.min.js",
+      format: "umd",
+      indent: false,
+      name: "druid",
+    },
+    onwarn,
+  },
+  {
+    input: "src/index.js",
+    plugins: [
+      json({
+        compact: true,
+      }),
+      resolve(),
+      commonjs(),
+    ],
+    output: {
+      sourcemap: true,
+      extend: true,
+      file: "dist/druid.cjs",
+      format: "cjs",
+      indent: false,
+      name: "druid",
+    },
+    onwarn,
+  },
 ];
